65a818831017c3636ce1153665245f2336db0138
nicoleli04 <nicoleliyh@gmail.com>
2023-08-11 13:30:26 -0700

    pipe
    pipe stuff

--- Flappy_Burd/Assets/Pipe_script.cs
+++ Flappy_Burd/Assets/Pipe_script.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pipe_script : MonoBehaviour
+
 {
+    public float moveSpeed;
+    public float deadZone = -45;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class Pipe_script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+
+        if(transform.position.x < deadZone)
+        {
+            Debug.Log("Pipe Deleted");
+            Destroy(gameObject);
+        }
     }
 }
