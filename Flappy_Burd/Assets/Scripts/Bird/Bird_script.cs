1504aa16d03dc0d87fd99c324ecf0a39b4519145
julieli334433 <83881461+julieli3344@users.noreply.github.com>
2023-08-13 09:16:00 -0700

    game over screen updated
    game over screen almost done, but there is one error.

--- Flappy_Burd/Assets/Scripts/Bird/Bird_script.cs
+++ Flappy_Burd/Assets/Scripts/Bird/Bird_script.cs
@@ -6,18 +6,28 @@ public class Bird_script : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
     public int velocity;
+    public UIGameOver logic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<UIGameOver>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-        {
+        {   
             myRigidbody.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (logic != null)
+        {
+            logic.GameOver();
         }
     }
 }
---
+++ Flappy_Burd/Assets/Scripts/UI/UIGameOver.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour
+{
+    public GameObject gameOverScreen;
+
+    [SerializeField] Button _playAgainGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_playAgainGame != null)
+        {
+            _playAgainGame.onClick.AddListener(LoadNewGame);
+        }
+        else
+        {
+            Debug.LogWarning("Play Again Button reference not assigned.");
+        }
+    }
+
+    private void LoadNewGame()
+    {
+        ScenesManager.Instance.LoadNewGame();
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
